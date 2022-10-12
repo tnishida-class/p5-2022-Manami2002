2e648ceb73f4a8b45486f37cc7bed10fa09204f2
Manami2002 <2170633h@stu.kobe-u.ac.jp>
2022-10-12 10:00:20 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,19 @@
 // チェッカー
 function setup() {
-  createCanvas(200, 200);
-  let size = width / 8;
+  createCanvas(400, 400);
   noStroke();
+  
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+      
+      rect(i*20, j*20,20,20);
+      if((i+j)%2==1 ) {
+        fill(196);
+      }
+      else{
+        fill(256);
+      } }
   }
 }
+
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
