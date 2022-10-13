0f1b4b8d0d0285518fe90d5bb0db6e2f4e82e3ad
Manami2002 <2170633h@stu.kobe-u.ac.jp>
2022-10-14 08:52:16 +0900

    change.section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,18 +1,30 @@
 // チェッカー
 function setup() {
   createCanvas(400, 400);
+  let size = width/8;
   noStroke();
   
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      
-      rect(i*20, j*20,20,20);
-      if((i+j)%2==1 ) {
-        fill(196);
+    
+      if((i+j)%2==0) {
+        fill(255);
       }
       else{
-        fill(256);
-      } }
+        fill(160);
+      } 
+      rect(i*size,j*size,size*(i+1),size*(j+1));
+
+     if(j<3&&(i+j)%2==1){
+      fill(255,0,0);
+      ellipse(size*(i+0.5),size*(j+0.5),46);
+    }
+      if(j>4&&(i+j)%2==1){
+        fill(9,10,23);
+        ellipse(size*(i+0.5),size*(j+0.5),46);
+      }
+    }
+      
   }
 }
 
